180d5c89e1882598a32cd92e17e1009d2f5b1d19
DavidWangwjh <davidwang.wjh@gmail.com>
2025-01-21 14:54:06 -0800

    Finished Handling user input

--- Landmarks/Views/Landmarks/LandmarkRow.swift
+++ Landmarks/Views/Landmarks/LandmarkRow.swift
@@ -15,24 +15,21 @@ struct LandmarkRow: View {
                 .resizable()
                 .frame(width: 50, height: 50)
             Text(landmark.name)
+            
             Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundStyle(.yellow)
+            }
         }
     }
 }
 
-#Preview("Turtle Rock") {
-    LandmarkRow(landmark: landmarks[0])
-}
-
-#Preview("Salmon") {
-    LandmarkRow(landmark: landmarks[1])
-}
-
 #Preview {
+    let landmarks = ModelData().landmarks
     Group {
         LandmarkRow(landmark: landmarks[0])
         LandmarkRow(landmark: landmarks[1])
     }
 }
-
-
