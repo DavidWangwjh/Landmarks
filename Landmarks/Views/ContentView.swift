bfb1b41a3353161516df2a3843a181c6bff354f1
DavidWangwjh <davidwang.wjh@gmail.com>
2025-01-21 02:12:24 -0800

    Finished Building lists and navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by David Wang on 1/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
