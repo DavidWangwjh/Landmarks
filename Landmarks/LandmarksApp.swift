f3413d29ff298fce0b568672583d87adef08cd99
DavidWangwjh <davidwang.wjh@gmail.com>
2025-01-20 22:15:27 -0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by David Wang on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
