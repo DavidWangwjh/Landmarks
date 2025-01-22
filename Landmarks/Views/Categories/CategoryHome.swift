148de8791d3a877034c1951c2a37a123d8e3db30
DavidWangwjh <davidwang.wjh@gmail.com>
2025-01-21 18:52:10 -0800

    Finished Composing complex interfaces

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,39 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by David Wang on 1/21/25.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    
+    @Environment(ModelData.self) var modelData
+    
+    var body: some View {
+        NavigationSplitView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environment(ModelData())
+}
